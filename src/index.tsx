b73fb50fbaabcb860f240908f17d6b0931a624ac
jcha0713 <joocha0713@gmail.com>
2023-04-11 21:09:43 +0900

    Test zod

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { useFetch } from "@raycast/utils";
+import { z } from "zod";
 
 export default function Command() {
+  const { isLoading, error, data: rawData } = useFetch("https://splatoon3.ink/data/schedules.json");
+  const regularSchedules = z.object({
+    startTime: z.string(),
+    endTime: z.string(),
+  });
+
+  const scheduleNodes = z.object({
+    nodes: z.array(regularSchedules),
+  });
+
+  const parsed = regularSchedules.parse(rawData.data.regularSchedules.nodes);
+  console.log(parsed);
+
   return (
     <List>
       <List.Item
